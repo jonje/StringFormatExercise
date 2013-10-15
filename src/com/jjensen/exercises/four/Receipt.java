d3cd872d16134773540127454351da1ceaa7c42b
jjensen <haiyun211@gmail.com>
2013-10-15 15:56:33 -0600

    Added variables for the width values in formatter

--- src/com/jjensen/exercises/four/Receipt.java
+++ src/com/jjensen/exercises/four/Receipt.java
@@ -10,23 +10,30 @@ import java.util.Formatter;
  * Time: 8:41 AM
  */
 public class Receipt {
+
+    private final int firstColumnWidth = 15;
+    private final int secondColumnWidth = 5;
+    private final int thirdColumnWidth = 10;
+    private final double specialWidth = 15.15;
+    private final double decimalPointValue = 10.2;
+
     private double total = 0;
     private Formatter f = new Formatter(System.out);
 
     public void printTitle(){
-        f.format("%-15s %5s %10s\n", "Item", "Qty", "Price");
-        f.format("%-15s %5s %10s\n", "----", "---", "-----");
+        f.format("%-" + firstColumnWidth + "s %" + secondColumnWidth + "s %" + thirdColumnWidth + "s\n", "Item", "Qty", "Price");
+        f.format("%-" + firstColumnWidth + "s %" + secondColumnWidth + "s %" + thirdColumnWidth + "s\n", "----", "---", "-----");
     }
 
     public void print(String name, int qty, double price) {
-        f.format("%15.15s %4d %10.2f\n", name, qty, price);
+        f.format("%-" +specialWidth + "s %" + secondColumnWidth + "d %" + decimalPointValue + "f\n", name, qty, price);
         total += price * qty;
     }
 
     public void printTotal(){
-        f.format("%-15s %5s %10.2f\n", "Tax", "", total * 0.06);
-        f.format("%-15s %5s %10s\n", "", "", "-----");
-        f.format("%-15s %5s %10.2f\n", "Total", "", total * 1.06);
+        f.format("%-" + firstColumnWidth + "s %" + secondColumnWidth + "s %" + decimalPointValue + "f\n", "Tax", "", total * 0.06);
+        f.format("%-" + firstColumnWidth + "s %" + secondColumnWidth + "s %" + thirdColumnWidth + "s\n", "", "", "-----");
+        f.format("%-" + firstColumnWidth + "s %" + secondColumnWidth + "s %" + decimalPointValue + "f\n", "Total", "", total * 1.06);
     }
 
 
